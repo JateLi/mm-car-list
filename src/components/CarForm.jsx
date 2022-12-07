597be5d598aeebb19ec6f5011f5483a5de9299f2
Jate Li <jate.li@vetradar.com>
2022-12-08 09:31:46 +1300

    Update submit button style

--- src/components/CarForm.jsx
+++ src/components/CarForm.jsx
@@ -75,7 +75,7 @@ function CarForm({ selectedCar, onSubmitChanges }) {
         </div>
 
         <button
-          className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded"
+          className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded disabled:opacity-50"
           type="submit"
           disabled={inputValidate(make, model, year, transmission)}
         >
